63fcd8edca1431db3ea4860fa562bb9e9eab39f5
ghvvl <valentin.logvinovitch@ya.ru>
2025-06-29 01:48:42 +0300

    [+] Refactored android implementation of PlatformCameraPreviewView to use LifecycleCameraController.

--- features/impl/addLoyaltyCard/src/androidMain/kotlin/com/vvl/loyalty_cards/features/impl/add_loyalty_card/view/PlatformCameraPreviewView.kt
+++ features/impl/addLoyaltyCard/src/androidMain/kotlin/com/vvl/loyalty_cards/features/impl/add_loyalty_card/view/PlatformCameraPreviewView.kt
@@ -1,30 +1,64 @@
 package com.vvl.loyalty_cards.features.impl.add_loyalty_card.view
 
 import android.view.ViewGroup
-import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.annotation.OptIn
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 import com.vvl.loyalty_cards.features.api.add_loyalty_card.component.AddLoyaltyCardComponent
-import com.vvl.loyalty_cards.features.impl.add_loyalty_card.utils.setupAnalysis
+import com.vvl.loyalty_cards.features.impl.add_loyalty_card.utils.mapToLoyaltyCardCodeType
+import java.util.concurrent.Executors
 
+@OptIn(ExperimentalGetImage::class)
+@Suppress("UnsafeCallOnNullableType")
 @Composable
 internal actual fun PlatformCameraPreviewView(component: AddLoyaltyCardComponent) {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
-    val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
-
-    DisposableEffect(cameraProviderFuture) {
-        onDispose {
-            cameraProviderFuture.get().unbindAll()
+    val scanner = remember {
+        BarcodeScanning.getClient(
+            BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+                .build()
+        )
+    }
+    val cameraController: LifecycleCameraController = remember {
+        LifecycleCameraController(context).apply {
+            setImageAnalysisAnalyzer(Executors.newSingleThreadExecutor()) { imageProxy ->
+                scanner
+                    .process(
+                        InputImage.fromMediaImage(
+                            imageProxy.image!!, imageProxy.imageInfo.rotationDegrees
+                        )
+                    )
+                    .addOnSuccessListener { barcodes ->
+                        if (barcodes.isNotEmpty()) {
+                            with(barcodes.first()) {
+                                component.onCodeReceived(
+                                    rawValue.orEmpty(),
+                                    format.mapToLoyaltyCardCodeType()
+                                )
+                            }
+                        }
+                    }
+                    .addOnCompleteListener { imageProxy.close() }
+            }
+            bindToLifecycle(lifecycleOwner)
         }
     }
-
     AndroidView(
         modifier = Modifier.fillMaxSize(),
         factory = { c ->
@@ -34,15 +68,15 @@ internal actual fun PlatformCameraPreviewView(component: AddLoyaltyCardComponent
                     ViewGroup.LayoutParams.MATCH_PARENT,
                 )
                 implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                controller = cameraController
             }
-        },
-        update = { previewView ->
-            setupAnalysis(
-                previewView = previewView,
-                cameraProviderFuture = cameraProviderFuture,
-                lifecycleOwner = lifecycleOwner,
-                onCodeReceived = component::onCodeReceived
-            )
         }
     )
+
+    DisposableEffect(Unit) {
+        onDispose {
+            cameraController.unbind()
+            scanner.close()
+        }
+    }
 }
